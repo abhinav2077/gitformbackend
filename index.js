9bf910413183d6dd2bd7d9cb3e208d241b23a0bf
Abhinav Jayalwaal <abhinavjayalwaal@Abhinavs-MacBook-Air.local>
2026-03-28 09:46:02 +0530

    added package.json

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ app.post('/upload', upload.single('file'), (req, res) => {
     }
   });
 
-  app.listen(3000, () => {
-    console.log('Server running on http://localhost:8000');
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
